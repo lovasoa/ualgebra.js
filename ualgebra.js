7cd8c8b5c7278562157bdc0549938ea157d89bf7
lovasoa <pere.jobs@gmail.com>
2012-06-11 03:43:04 +0300

    Add gauss-jordan elimination algorithm. It's much more efficient than the previous algorithm to inverse large matrices (takes 1/10th of the time it took with a 6*6 matrix).

--- ualgebra.js
+++ ualgebra.js
@@ -1,17 +1,13 @@
 // The MIT License
-
 // Copyright (c) 2011 Pedro http://lamehacks.net
-
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,82 +16,86 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+"use strict";
 
-
-function matrixMultiply(a,b){
-	var btrans = transposeMatrix(b);
-	var result = [];
-	for(var i=0; i < a.length; i++){
-		var row = [];
-		for(var j=0; j < btrans.length; j++){
-			var value = internalProduct(a[i],btrans[j]);
-			row.push(value)
-		}
-		result.push(row);
-	}
-	return result;
+function matrixMultiply(a, b) {
+    var btrans = transposeMatrix(b);
+    var result = [];
+    for (var i = 0; i < a.length; i++) {
+        var row = [];
+        for (var j = 0; j < btrans.length; j++) {
+            var value = internalProduct(a[i], btrans[j]);
+            row.push(value);
+        }
+        result.push(row);
+    }
+    return result;
 }
 
-function matrixScalarMultiply(m,s){
-	var result = [];
-	for(var i=0; i < m.length; i++){
-		var row = [];
-		for(var j=0; j < m[0].length; j++){
-			row.push(s * m[i][j]);
-		}
-		result.push(row);
-	}
-	return result;
+function matrixScalarMultiply(m, s) {
+    var result = [];
+    for (var i = 0; i < m.length; i++) {
+        var row = [];
+        for (var j = 0; j < m[0].length; j++) {
+            row.push(s * m[i][j]);
+        }
+        result.push(row);
+    }
+    return result;
 }
 
 
 //unnecessary - use dotOp instead
-function matrixAdd(a,b){
-	var result = [];
-	for(var i=0; i < a.length; i++){
-		var row = [];
-		for(var j=0; j < a[0].length; j++){
-			row.push(a[i][j] + b[i][j]);
-		}
-		result.push(row);
-	}
-	return result;
+
+
+function matrixAdd(a, b) {
+    var result = [];
+    for (var i = 0; i < a.length; i++) {
+        var row = [];
+        for (var j = 0; j < a[0].length; j++) {
+            row.push(a[i][j] + b[i][j]);
+        }
+        result.push(row);
+    }
+    return result;
 }
 
 //for internal usage only
-function internalProduct(u,v){
-	if (u.length != v.length) throw "SizesDoNotMatch";
-	var sum = 0;
-	for(var i=0; i < u.length; i++){
-		sum += u[i]*v[i];
-	}
-	return sum;
+
+
+function internalProduct(u, v) {
+    if (u.length != v.length) throw "SizesDoNotMatch";
+    var sum = 0;
+    for (var i = 0; i < u.length; i++) {
+        sum += u[i] * v[i];
+    }
+    return sum;
 }
 
-function transposeMatrix(m){
-	var t = [];
-	for(var i=0; i < m[0].length; i++){
-		var row = [];
-		for(var j=0; j < m.length; j++){
-			row.push(m[j][i]);
-		}
-		t.push(row);
-	}
-	return t;
+function transposeMatrix(m) {
+    var t = [];
+    for (var i = 0; i < m[0].length; i++) {
+        var row = [];
+        for (var j = 0; j < m.length; j++) {
+            row.push(m[j][i]);
+        }
+        t.push(row);
+    }
+    return t;
 }
 
-function minorMatrix(m, k, l){
-	var reduced = [];
-	for(var i=0; i < m.length; i++){
-		if(i==k) continue;
-		var row = [];
-		for(var j=0; j < m.length; j++){
-			if(j==l) continue;
-			row.push(m[i][j])
-		}
-		reduced.push(row);
-	}
-	return reduced;
+function minorMatrix(m, k, l) {
+    var reduced = [];
+    for (var i = 0; i < m.length; i++) {
+        if (i == k) continue;
+        var row = [];
+        for (var j = 0; j < m.length; j++) {
+            if (j == l) continue;
+            row.push(m[i][j]);
+        }
+        reduced.push(row);
+    }
+    return reduced;
 }
 
 
@@ -103,42 +103,45 @@ function minorMatrix(m, k, l){
 Recursive implementation using laplace expansion
 http://www.webcitation.org/61AGedZlm
 */
-function determinant(m){
-	var size = m.length;
-	if(size == 1) return m[0][0];
-	if(size == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
-	var det = 0;
-	for(var i=0; i < size; i++){
-		var minor = minorMatrix(m,0,i);
-		var signal = (i%2 > 0) ? -1 : 1;
-		det += signal * m[0][i]* determinant(minor);
-	}
-	return det;
+
+function determinant(m) {
+    var size = m.length;
+    if (size == 1) return m[0][0];
+    if (size == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+    var det = 0;
+    for (var i = 0; i < size; i++) {
+        var minor = minorMatrix(m, 0, i);
+        var signal = (i % 2 > 0) ? -1 : 1;
+        det += signal * m[0][i] * determinant(minor);
+    }
+    return det;
 }
 
 
 /*
 http://en.wikipedia.org/wiki/Cofactor_(linear_algebra)
 */
-function cofactor(m, k, l){
-	minor = minorMatrix(m, k, l);
-	return determinant(minor);
+
+function cofactor(m, k, l) {
+    var minor = minorMatrix(m, k, l);
+    return determinant(minor);
 }
 
 /*
 http://en.wikipedia.org/wiki/Cofactor_(linear_algebra)#Matrix_of_cofactors
 */
-function cofactorMatrix(m){
-	var cofactors = [];
-	for(var i = 0; i < m.length; i++){
-		var row = [];
-		for(var j = 0; j < m.length; j++){
-			var cofactorval = cofactor(m,i,j);
-			row.push(cofactorval);
-		}
-		cofactors.push(row);
-	}
-	return cofactors;
+
+function cofactorMatrix(m) {
+    var cofactors = [];
+    for (var i = 0; i < m.length; i++) {
+        var row = [];
+        for (var j = 0; j < m.length; j++) {
+            var cofactorval = cofactor(m, i, j);
+            row.push(cofactorval);
+        }
+        cofactors.push(row);
+    }
+    return cofactors;
 }
 
 
@@ -146,31 +149,32 @@ function cofactorMatrix(m){
 Used the adjoint method
 http://www.webcitation.org/61BTRqAoZ
 */
-function inverseMatrix(m){
-	var det = determinant(m);
-	if (det == 0) throw "SingularMatrix";
-	var deti = 1 / det;
-	var cof = cofactorMatrix(m);
-	var adj = transposeMatrix(cof);
-	var result = matrixScalarMultiply(adj,deti);
-	return result;
+
+function inverseMatrix(m) {
+    var det = determinant(m);
+    if (det === 0) throw "SingularMatrix";
+    var deti = 1 / det;
+    var cof = cofactorMatrix(m);
+    var adj = transposeMatrix(cof);
+    var result = matrixScalarMultiply(adj, deti);
+    return result;
 }
 
 /*
 performs operation element by element between to matrices
 */
-function dotOp(func,m,n){
-	var result = [];
-	for(var i = 0; i < m.length; i++){
-		var row = [];
-		for(var j = 0; j < m[0].length; j++){
-			row.push(func(m[i][j], n[i][j]));
-		}
-		result.push(row);
-	}
-	return result;
-}
 
+function dotOp(func, m, n) {
+    var result = [];
+    for (var i = 0; i < m.length; i++) {
+        var row = [];
+        for (var j = 0; j < m[0].length; j++) {
+            row.push(func(m[i][j], n[i][j]));
+        }
+        result.push(row);
+    }
+    return result;
+}
 
 /*Returns the matrix obtained by applying the function func
 on each couple of factors m(i,k) and m(j,k).
@@ -213,31 +217,105 @@ function rowMultiply (m, i, lambda) {
     });
 }
 
+/*Use the Gauss-Jordan elimination on m.
+If the matrix n is provided, the same row operations will be applied to n.
+So if n is the identity matrix, it will become the inverse of m*/
+function gauss_jordan_elimination (m, n) {
+    var curRow, curColumn, h=m.length, w=m[0].length;
+    for (curColumn=0; curColumn<Math.min(w,h); curColumn++) {
+        //Find the row on which we will start the elimination
+        for (var i=curColumn; i<h; i++) {
+            if (m[i][curColumn]!==0) break;
+        }
+        if (i<h) curRow=i;
+        else continue;
+        swapRows(m, curRow, curColumn);
+        if (n) swapRows(n, curRow, curColumn);
+        curRow = curColumn;
 
-function generateMatrix(nlines, ncols, func){
-	var m = [];
-	for(var i = 0; i < nlines; i++){
-		var row = [];
-		for(var j = 0; j < ncols; j++){
-			row.push(func(i,j));
-		}
-		m.push(row);
-	}
-	return m;
+    	var lambda =  1/m[curRow][curColumn];
+		rowMultiply(m, curRow, lambda);
+        if (n) rowMultiply(n, curRow, lambda);
+
+        for (var i=0; i<h; i++) {
+        	if (i==curRow) continue;
+            var lambda = m[i][curColumn]/m[curRow][curColumn];
+            substractAndMultiply(m, i, curRow, lambda); 
+            if (n) substractAndMultiply(n, i, curRow, lambda);
+        }
+    }
+    return m;
 }
 
-function zeros(nlines,ncols){
-	return generateMatrix(nlines,ncols, function(i,j){return 0;});
+function fasterInverse (m) {
+    var size=0;
+    if (m.length != m[0].length) throw "SingularMatrix";
+    else size = m.length;
+    var mcopy = clone(m);
+    var inverse = identity(size);
+    gauss_jordan_elimination(mcopy, inverse);
+    return inverse;    
 }
 
-function ones(nlines,ncols){
-	return generateMatrix(nlines,ncols, function(i,j){return 1;});
+/*Solve A.x = B*/
+function linearSolve(A, B) {
+    var Acopy = clone(A);
+
+    var Bcopy;
+	if (typeof(B[0])==="number") Bcopy = vector(B);
+    else if (B) Bcopy = clone(B);
+    else Bcopy = zeros(A.length, 1);
+
+    gauss_jordan_elimination(Acopy, Bcopy);
+    return Bcopy;
 }
 
-function identity(size){
-	return generateMatrix(size, size, function(i,j){ if(i===j) return 1; return 0; });
+function generateMatrix(nlines, ncols, elements) {
+    var func;
+    if (typeof(elements) === "number"){
+        func = function(){return elements;};
+    }else{
+        func = elements;
+    }
+    var m = [];
+    for (var i = 0; i < nlines; i++) {
+        var row = [];
+        for (var j = 0; j < ncols; j++) {
+            row.push(func(i, j));
+        }
+        m.push(row);
+    }
+    return m;
 }
 
+function clone(m) {
+    var h=m.length, w=m[0].length;
+    return generateMatrix(h, w, function(i, j) {
+        return m[i][j];
+    });
+}
+
+function vector(arr) {
+    var h=arr.length;
+    return generateMatrix(h, 1, function(i) {
+        return arr[i];
+    });
+}
+
+function zeros(nlines, ncols) {
+    return generateMatrix(nlines, ncols, 0);
+}
+
+function ones(nlines, ncols) {
+    return generateMatrix(nlines, ncols, 1);
+}
+
+function identity(size) {
+    return generateMatrix(size, size, function(i, j) {
+        if (i === j) return 1;
+        else return 0;
+    });
+}
 
 function randomNumberInRange (min, max) {
     return min + (max-min)*Math.random();
@@ -252,7 +330,6 @@ function randomMatrix (nlines, ncols, min, max) {
         return randomNumberInRange(min, max);
     });
 }
-
 
 function matrix2str (m) {
 	var w=m[0].length, h=m.length;
